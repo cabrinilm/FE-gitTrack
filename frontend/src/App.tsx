d8b429b593eebe2ad0c078ce9e9840e2c7575c01
cabrinilm <luca_cabrinimm@hotmail.com>
2026-02-06 20:16:30 +0000

    tailwind working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,12 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-red-500 p-10">
-      <h1 className="text-white text-4xl font-bold">
-        Fundo vermelho = Tailwind está funcionando?
+    <div className="min-h-screen flex items-center justify-center bg-neutral-900">
+      <h1 className="text-4xl font-bold text-lime-400">
+        Tailwind v4 OK 🚀
       </h1>
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
