7c88242ab536fed6bb59c46d09ad3d31de29fd3f
cabrinilm <luca_cabrinimm@hotmail.com>
2026-02-06 19:56:57 +0000

    vite download

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 p-10">
+      <h1 className="text-white text-4xl font-bold">
+        Fundo vermelho = Tailwind está funcionando?
+      </h1>
+    </div>
+  );
+}
+export default App;
