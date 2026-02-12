c114466b0a3975bc7bbbdbd6f44d3d56f0c29337
cabrinilm <luca_cabrinimm@hotmail.com>
2026-02-12 19:19:20 +0000

    context created

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -4,3 +4,24 @@ import { createContext, useState } from "react";
 import type { User, Session, AuthError } from "@supabase/supabase-js";
 // type 
 
+
+type AuthResponse = {
+  user: User | null
+  session: Session | null
+  error: AuthError | null
+};
+
+interface AuthContextValue {
+    user: User | null,
+    isLoading: boolean, 
+    error: string | null
+    signIn: (email: string, password: string) => Promise<AuthResponse>
+    signOut: () => Promise<void>
+    signUp: (
+        email: string, 
+        password: string,
+        username: string, 
+    ) => Promise<AuthResponse>
+};
+
+export const AuthContext = createContext<AuthContextValue | null>(null)
