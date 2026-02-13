54b4d72f1a2f998e28e99508d6aa9d81aca3e582
cabrinilm <luca_cabrinimm@hotmail.com>
2026-02-13 18:29:59 +0000

    authprovider initiate

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,3 +1,25 @@
-import type { useState, useEffect, ReactNode } from "react";
+import { useState, type useEffect, type ReactNode } from "react";
 import { AuthContext } from "./AuthContext";
+import type { User } from "@supabase/supabase-js";
 
+interface AuthProviderProps {
+    children : ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [isLoading, setIsLoanding] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+  
+  
+  
+    // ...
+
+
+
+  return (
+    <AuthContext.Provider value={/* algo */}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
