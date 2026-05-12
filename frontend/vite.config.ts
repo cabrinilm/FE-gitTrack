55816b35f5b62fd7f5cd47af74d914c51021d558
cabrinilm <luca_cabrinimm@hotmail.com>
2026-05-13 00:40:17 +0100

    fix(build): configure manual path alias in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  // you can remove the manual alias now
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
-
